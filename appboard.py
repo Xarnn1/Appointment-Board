337a4c731e7ba03483bcc5cc036336082c25d48e
Jason Morrissette <j.morrissette@jamdevs.com>
2018-03-23 09:17:52 -0700

    Added Add appointments feature, and a public display page.

---
+++ appboard.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+
+def public():
+    return render_template('public_display.html')
+
+@app.route('/add')
+
+def add():
+    return render_template('add_appt.html')
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0")
---
+++ appboard.wsgi
@@ -0,0 +1,6 @@
+
+import sys
+
+sys.path.insert(0,'/var/www/appboard')
+
+from appboard import app as application
