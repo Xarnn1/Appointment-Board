7b8f1c8d230a9b9a2565a8ba3850df9dafddbfd2
Jason Morrissette <j.morrissette@jamdevs.com>
2018-04-17 10:18:15 -0700

    Feat: Created Stylesheet

--- appboard.py
+++ appboard.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHMEY_DATABASE_URI'] = 'sqlite:///appt.db'
+app.secret_key = 'null' # change on deployment
+
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 
